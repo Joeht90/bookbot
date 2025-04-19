a68c01b4008a939c8a2a06c55de86d8c4654e7e8
joeht90 <joeht90@icloud.com>
2025-04-19 15:33:40 -0700

    Changes made to print out a report

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 from stats import get_word_count
 from stats import get_character_count
+from stats import make_new_list
+from stats import sort_on
+
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
 
 
 def main():
-   print(f"{get_character_count("books/frankenstein.txt")}")
+    book = get_book_text("books/frankenstein.txt")
+    word_count = get_word_count(book)
+    book_dict = get_character_count(book)
+    book_list = make_new_list(book_dict)
+    book_list.sort(reverse=True, key=sort_on)
+    print(f"Found {word_count} total words")
+    for i in range(len(book_list)):
+        print(f"{book_list[i]["character"]}: {book_list[i]["count"]}")
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,27 +1,32 @@
-def get_word_count(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    book_list = f.split()
+def get_word_count(book):
+    book_list = book.split()
     return len(book_list)
 
 
-def get_character_count(filepath):
+def get_character_count(book):
     # Create blank dictionary
     character_counts = {}
-    # With block to open file
-    with open(filepath) as f:
-        # create a variable of the file as a string
-        file_contents = f.read()
         # For loop to iterate through each string index
-    for i in file_contents:
+    for i in book:
         # Increment dictionary for each string and make sure to 
         # .lower() the iteration to avoid duplicates
         # Had to add an if statement was unable to create a new key value
         # while incrementing
-        if i.lower() in character_counts:
-            character_counts[i.lower()] += 1
-        else:
-            character_counts[i.lower()] = 1
+        if i.isalpha():
+            if i.lower() in character_counts:
+                character_counts[i.lower()] += 1
+            else:
+                character_counts[i.lower()] = 1
     return character_counts
 
+
+def sort_on(dict):
+    return dict["count"]
+
+
+def make_new_list(dict):
+    new_list = []
+    for i in dict:
+        new_list.append({"character": i, "count": dict[i]})
+    return new_list
 
