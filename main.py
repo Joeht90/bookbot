260a2e649b47b750727d24db150de85fe60067ef
Joeht90 <joeht90@gmail.com>
2025-10-29 19:19:40 -0700

    kept updating main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+
+def get_word_count(text):
+    return len(text.split())
+
+
+def main():
+    count = get_word_count(get_book_text("books/frankenstein.txt"))
+    print(f"Found {count} total words")
+
+
+main()
